f1f91378100f1bbbd72d5854371b983f1896e789
alnaamani <ah666052@dal.ca>
2024-08-16 18:35:08 +0400

    Completed Navbar Development

--- alnaamanilogistics/src/components/Navbar.jsx
+++ alnaamanilogistics/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import '../styles/Navbar.css';
 import Logo from '../images/logo.png';
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from  'react-scroll';
 
 function Navbar(){
+    const[isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <nav className='navbar'>
             {/* Logo */}
@@ -11,13 +16,18 @@ function Navbar(){
                 <img src={Logo} alt='AlNaamani Logistics Logo'/>
                 <span className='logo-text'>AL NAAMANI LOGISTICS</span>
             </div>
-
+             {/* Hamburger Menu */}
+             <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
              {/* Navigation Links */}
-             <ul className='navbar-links'>
+             <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
                 <li><Link activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={500}>Home</Link></li>
-                <li><Link activeClass="active" to="services" spy={true} smooth={true} offset={-70} duration={500}>Our Services</Link></li>
-                <li><Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500}>About Us</Link></li>
-                <li><Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>Contact Us</Link></li>
+                <li><Link activeClass="active" to="services" spy={true} smooth={true} offset={-70} duration={500}>Services</Link></li>
+                <li><Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500}>About</Link></li>
+                <li><Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>Contact</Link></li>
              </ul>
         </nav>
     );
